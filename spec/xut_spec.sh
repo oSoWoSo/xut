c3cd25964967aaed79ce6477fbc8905837e7e939
AngMits <65828210+AngMits@users.noreply.github.com>
2025-10-09 22:08:43 +0300

    Add shellspec configuration and woodpecker CI file
    - Squashed to clean up history
    
    Co-Authored-By: mobinmob <mobinmob@noreply.codeberg.org>

---
+++ spec/xut_spec.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+Describe 'xut basic usage'
+  It 'shows usage when no arguments'
+    When run script ./xut
+    The stdout should include 'Usage:'
+    The status should be success
+  End
+
+  It 'shows error when run as root'
+    When run script ./xut
+    The stdout should include 'Error:'
+    The status should be failure
+  End
+
+  It 'shows help with -h'
+    When run script ./xut -h
+    The stdout should include 'OPTIONS'
+    The status should be success
+  End
+End
