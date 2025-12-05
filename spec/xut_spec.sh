b0e4db0144a29bb6ed35f9f798597450fbb2611e
zenobit <zenobit@disroot.org>
2025-12-05 06:33:27 +0100

    CI: spec

--- spec/xut_spec.sh
+++ spec/xut_spec.sh
@@ -4,11 +4,11 @@ Describe 'xut basic usage'
   It 'shows usage when no arguments'
     When run script ./xut
     The stdout should include 'Usage:'
-    The status should be success
+    The status should be failure
   End
 
   It 'shows error when run as root'
-    When run script ./xut
+    When run script sudo ./xut
     The stdout should include 'Error:'
     The status should be failure
   End
